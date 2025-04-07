1811711611abc05052e5b0ef914a1bbe4dc31cb2
Jacob-Neill <neillrjacob@gmail.com>
2025-04-07 18:55:25 -0500

    Remove useless code

--- matrix.hpp
+++ matrix.hpp
@@ -18,16 +18,5 @@ namespace std {
 		struct Matrix <T, size, sizes...> {
 			using type = array<matrix<T, sizes...>, size>;
 		};
-
-		//This can be used to make a vector version but the values still have to be known at compile time so it's kinda useless
-		/*template<typename T, size_t size>
-		inline matrix<T, size> make_matrix(T item = {}) {
-			return vector<T>(size, item);
-		}
-
-		template<typename T, size_t size, size_t... sizes>
-		inline matrix<T, size, sizes...> make_matrix(T item = {}) {
-			return vector<matrix<T, sizes...>>(size, make_matrix<T, sizes...>(item));
-		}*/
 	}
 }
